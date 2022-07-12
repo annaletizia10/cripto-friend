74d0b7b318289c1254a7188a2096970605e9d1d4
Anna Moreno <annaletizia10@hotmail.com>
2022-07-11 19:27:57 -0500

    updated paths

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ export default function App(){
     return (
         <div>
             <Routes>
-                <Route exact path="/cripto-friend" element={
+                <Route exact path="/cripto-friend/" element={
                     <Welcome 
                         handleChange={handleChange}
                         formData={formData}
@@ -70,7 +70,7 @@ export default function App(){
                         formErrors={formErrors}
                         success={success}
                     />} />
-                <Route path="cripto-friend/dashboard" element={
+                <Route path="/cripto-friend/dashboard" element={
                     <Dashboard
                         userData={formData}
                         cryptoData={cryptoData}
